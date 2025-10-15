f7424a264e129f83983a4e5d9225a27b2099700a
Rana <ranaeng500238@gmail.com>
2025-10-15 13:10:18 +0300

    create file check_Out.html & checkOut.js and add a form to payment steps

---
+++ js/check_out.js
@@ -0,0 +1,8 @@
+// form validation
+let fname = document.getElementById("fname").value.trim();
+let lname = document.getElementById("lname").value.trim();
+const usrname = fname + " " + lname;
+let email=document.getElementById("email").value.trim();
+let phone=document.getElementById("phone").value.trim();
+let address=document.getElementById("address").value.trim();
+let payMethod=document.getElementById("payment").value;
