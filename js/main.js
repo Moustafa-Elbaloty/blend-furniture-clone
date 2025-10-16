b084a9e40a3ddff40629f90565ae335ae34c2c47
Moustafa elbaloty <moustafaelbaloty@gmail.com>
2025-10-16 08:24:13 +0300

    Enhance menu and image features in main.js
    Added functionality for dropdown list and improved image handling.

--- js/main.js
+++ js/main.js
@@ -11,13 +11,8 @@ closeBtn.addEventListener("click", () => {
     list.style.transform = "scale(0)"
 })
 
-// style for background landing
-let backGround = document.querySelector(".land-img")
-backGround.style.width = "100vw"
-backGround.parentElement.style.cssText = "position:relative;overflow:hidden"
-backGround.nextElementSibling.style.cssText = `position:absolute;top: 50%;z-index: 10;left: 50%;width: 100vw;transform: translate(-50%,-50%);
-`
-/// droplist for prosducts
+
+/// ****************droplist for prosducts*********
 let products = document.querySelector(".products")
 let dropdownList = document.querySelector(".dropdown-List")
 dropdownList.style.display = "none"
@@ -30,7 +25,7 @@ let items = [
     { name: "fierplaces", link: "fierplaces.html" },
     { name: "classic", link: "classic.html" },
 ];
-
+// create element in droplist
 items.forEach(items => {
     let li = document.createElement("li")
     let a = document.createElement("a")
@@ -46,6 +41,7 @@ products.addEventListener("click", function () {
     dropdownList.classList.toggle("hidden")
 })
 
+
 let links = document.querySelectorAll(".links a")
 links.forEach((a) => {
     a.addEventListener("click", (e) => {
@@ -56,6 +52,12 @@ links.forEach((a) => {
     })
 }
 )
+// style for background landing
+let backGround = document.querySelector(".land-img")
+backGround.style.width = "100%"
+backGround.parentElement.style.cssText = "position:relative;overflow:hidden"
+backGround.nextElementSibling.style.cssText = `position:absolute;top: 50%;z-index: 10;left: 50%;width: 100vw;transform: translate(-50%,-50%);
+`
 
 ///************wrap image****************///
 let images = [
@@ -82,7 +84,7 @@ images.forEach(image => {
 let feature = document.querySelector(".feature")
 let heading = document.createElement("img")
 feature.style.cssText = `text-align:center;padding:100px 0;`
-heading.style.cssText = `transform: translateY(70px);transition:.5s`
+heading.style.cssText = `max-width: 100%;transform: translateY(70px);transition:.5s`
 heading.className = "head"
 heading.src = 'images/Home/home10.jpg'
 feature.prepend(heading)
@@ -97,3 +99,15 @@ const observHead = new IntersectionObserver(e => {
     })
 })
 observHead.observe(heading)
+// create cards
+let featContainer = document.querySelector(".feature .constainer")
+let cards = document.createElement("div")
+cards.className = "cards"
+let cardImg = [
+    { src: "images/", data: "sofas" },
+    { src: "images/Home/home07.jpg", data: "recliners" },
+    { src: "images/Home/home08.jpg", data: "dinin Tables" },
+    { src: "images/Home/home05.jpg", data: "chairs" },
+    { src: "images/Home/home06.jpg", data: "coffee tables" }
+];
+cardImg
